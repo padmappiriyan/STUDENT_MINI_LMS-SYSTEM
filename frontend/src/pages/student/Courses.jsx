83aa69f1620f771ad037859a67bb0ba02e388852
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-12-19 20:24:09 +0530

    Courses Page

---
+++ frontend/src/pages/student/Courses.jsx
@@ -0,0 +1,20 @@
+
+import AvailableCourse from '../../components/student/AvailableCourse';
+
+const StudentCourses = () => {
+ 
+
+
+
+
+  return (
+
+     <div className='w-full h-full'>
+          <AvailableCourse />
+
+     </div>
+   
+  );
+};
+
+export default StudentCourses;
